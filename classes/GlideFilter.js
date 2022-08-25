4c75ea12cd3c4fa02ef499a5809a2dcb420bca32
Luke Van Epen <vanweapon@gmail.com>
2022-08-25 19:00:18 +1000

    adding GlideFilter file

---
+++ classes/GlideFilter.js
@@ -0,0 +1,11 @@
+
+class GlideFilter {
+
+    /**
+     * Checks a given glide record against an encoded query to check if it matches. Note that this method is CASE-SENSITIVE
+     * @param {GlideRecord} GlideRecord a valid {@link GlideRecord} to check 
+     * @param {string} encodedQuery an encoded query to validate against the glide record
+     * 
+     */
+    static checkRecord(GlideRecord, encodedQuery) { }
+}
