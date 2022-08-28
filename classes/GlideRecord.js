ac46dbd3b2fa2d7f60b400a957b0e0ec0916d23b
Luke Van Epen <vanweapon@gmail.com>
2022-08-28 14:10:39 +1000

    added createElement and updateElement

--- classes/GlideRecord.js
+++ classes/GlideRecord.js
@@ -28,7 +28,7 @@ class GlideRecord {
      * @example
      * var gr = new GlideRecord("incident");
      * gr.get("abc123");
-     * 
+     * @example
      * var gr = new GlideRecord("incident");
      * gr.get("number", "INC0001234");
      */
@@ -53,24 +53,24 @@ gs.info(gr.isValidEncodedQuery()); //false
      * 
      * @returns {undefined} in the following cases
      * @example
-var gr = new GlideRecord("incident");
-gr.addQuery("active",true);
-gs.info(gr.largeResultExpected()); //undefined
-
-var gr = new GlideRecord("incident");
-gr.addQuery("active",true);
-gr.query()
-gs.info(gr.largeResultExpected()); //undefined
-
-var gr = new GlideRecord("incident");
-gr.addQuery("active",true);
-gr.query();
-gr.next();
-gs.info(gr.largeResultExpected()); //undefined
-
-var gr = new GlideRecord("sys_metadata");
-...
-gs.info(gr.largeResultExpected()); //undefined
+     * var gr = new GlideRecord("incident");
+     * gr.addQuery("active",true);
+     * gs.info(gr.largeResultExpected()); //undefined
+     * @example
+     * var gr = new GlideRecord("incident");
+     * gr.addQuery("active",true);
+     * gr.query()
+     * gs.info(gr.largeResultExpected()); //undefined
+     * @example
+     * var gr = new GlideRecord("incident");
+     * gr.addQuery("active",true);
+     * gr.query();
+     * gr.next();
+     * gs.info(gr.largeResultExpected()); //undefined
+     * @example
+     * var gr = new GlideRecord("sys_metadata");
+     * ...
+     * gs.info(gr.largeResultExpected()); //undefined
 
 
      */
@@ -97,7 +97,7 @@ gs.info(gr.largeResultExpected()); //undefined
     isReadOnly() { }
 
     /**
-     * only appears once in the platform in the Relationship for Audit
+     * Only appears once in the platform in the Relationship for Audit.
      * It looks to be used for tables with a Table Rotation set up, where you want to filter your top-level table down to just the table rotation for performance reasons. 
      * 
      * @summary Undocumented method
@@ -127,12 +127,58 @@ gs.info(gr.largeResultExpected()); //undefined
      */
     getEncodedQuery() { }
 
+    /**
+     * Returns the internal table name of the GlideRecord. Always returns the subclass even if the GR was queried from a higher table 
+     * @summary Documented method
+     * 
+     * @example
+     * var inc = new GlideRecord("incident");
+     * inc.addNotNullQuery("short_description")
+     * inc.orderBy("short_description");
+     * inc.query();
+     * 
+     * inc.next();
+     * gs.info(inc.number + " - " + inc.getRecordClassName()); //INC0008112 - incident
+     * @example
+     * var task = new GlideRecord("task");
+     * task.get("number", "INC0008112");
+     * gs.info(task.number + " - " + task.getRecordClassName());// INC0008112 - incident
+     */
     getRecordClassName() { }
 
+    /**
+     * Seen in script include DeliveryPlanTaskGenerator along with {@link GlideRecord#popCurrent} to run a script evaluation which generates child tasks for a parent using a delivery plan.
+     * Since delivery plans are no longer used in new versions, this is probably a deprecated feature. 
+     * @summary Unknown method, possibly deprecated
+     * 
+     * @example 
+// global.DeliveryPlanTaskGenerator line 135
+   _runTaskScript : function (
+   task, //GlideRecord
+   tt //TaskToken
+   ) {
+    if (!tt.getGenerationScript())
+        return;
+
+    task.putCurrent();
+    GlideEvaluator.evaluateString(tt.getGenerationScript() + '');
+    task.popCurrent();
+}
+     */
     putCurrent() { }
 
+    /**
+     * See {@link GlideRecord#putCurrent}
+     * @summary Unknown method, possibly deprecated
+     */
     popCurrent() { }
 
+    /**
+     * Only used once in the platform in the business rule CreateElement. Function appears to be related to creating supplementry files as part of the insert of a sys_dictionary record.
+     * 
+     * @summary Unknown function
+     * @see /nav_to.do?uri=sys_script.do?sys_id=75b83f9fc6112271000396c74dfbab7a
+     */
     createElement() { }
 
     updateElement() { }
@@ -315,7 +361,33 @@ gs.info(gr.largeResultExpected()); //undefined
 
     getEngineParameter() { }
 
-    setUseEngines() { }
+    /**
+     * Controls whether engines such as approval engines will trigger upon calling .update() or .insert() on this record.
+     * Anything from this list marked as an 'engine' will not run: {@link https://docs.servicenow.com/bundle/tokyo-application-development/page/script/general-scripting/reference/r_ExecutionOrderScriptsAndEngines.html}
+     * 
+     * For example, this can be used to turn off Data Policies before inserting a record with empty fields which would normally be mandatory at a db level. 
+     * @summary Undocumented method
+     * 
+     * @param {boolean} useEngines If false, disables the use of engines for the next insert/update. 
+     * 
+     * @see https://community.servicenow.com/community?id=community_question&sys_id=4caa472ddb5cdbc01dcaf3231f96191b
+     * @example
+     * // Prevent running engines during insert of a record
+     * var chg = new GlideRecord("change_request");
+     * chg.newRecord();
+     * chg.setUseEngines(false);
+     * chg.insert();
+     * 
+     * @example
+     * // Turn off engines for one update, then re-enable for a second update
+     * task.setUseEngines(false);
+     * task.setValue('state',3);
+     * task.update(); // Engines will not run for this update
+     * task.setUseEngines(true);
+     * ...
+     * task.update(); //engines will run for this update 
+     */
+    setUseEngines(useEngines) { }
 
     setWorkflow() { }
 
